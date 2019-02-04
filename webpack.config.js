87038f786c09fe0b3bff89027a0a9f545d71231b
Adam Rubock <adamrubock1@gmail.com>
2019-02-03 23:51:37 -0500

    Merge webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	resolve: { extensions: ['.js', '.jsx'] },
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			}
+		]
+	}
+};
