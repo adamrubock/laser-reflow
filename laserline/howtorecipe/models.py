984f8fd75bda0f4a778fa6bda3fd3a5f8448920b
Adam Rubock <adamrubock1@gmail.com>
2019-03-21 10:59:55 -0400

    Created functional models/serializers

---
+++ laserline/howtorecipe/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Recipe(models.Model):
+	recipe_name = models.CharField(max_length=100,unique=True)
+	x_width = models.FloatField(validators=[MinValueValidator(0.0),MaxValueValidator(1.0)])
+	y_width = models.FloatField(validators=[MinValueValidator(0.0),MaxValueValidator(1.0)])
+
+	def __str__(self):
+		return self.recipe_name
+
+class TimePoint(models.Model):
+	timepoint_duration = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10000000)])
+	power_level = models.FloatField(validators=[MinValueValidator(0.0),MaxValueValidator(1.0)])
+	recipe = models.ForeignKey(Recipe,on_delete=models.CASCADE,related_name='timepoints')
---
+++ laserline/howtorecipe/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+from .models import TimePoint, Recipe
+
+class TimePointSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = TimePoint
+		fields=('id','timepoint_duration','power_level')
+
+class RecipeSerializer(serializers.ModelSerializer):
+	timepoints = TimePointSerializer(many=True)
+
+	class Meta:
+		model = Recipe
+		fields=('id','recipe_name','x_width','y_width','timepoints')
+
+	def create(self, validated_data):
+		new_recipe = Recipe()
+		new_recipe.recipe_name = validated_data.get('recipe_name')
+		new_recipe.x_width = validated_data.get('x_width')
+		new_recipe.y_width = validated_data.get('y_width')
+		new_recipe.save()
+		
+		recipe_timepoints = validated_data.get('timepoints')
+		bulk_timepoints = []
+		for timepoint in recipe_timepoints:
+			new_timepoint = TimePoint(recipe=new_recipe,**timepoint)
+			bulk_timepoints.append(new_timepoint)
+
+		TimePoint.objects.bulk_create(bulk_timepoints)
+		return new_recipe
+	
+	def update(self,instance,validated_data):
+		instance.recipe_name=validated_data.get('recipe_name')
+		instance.x_width=validated_data.get('x_width')
+		instance.y_width=validated_data.get('y_width')
+		TimePoint.objects.filter(recipe=instance).delete()
+		recipe_timepoints = validated_data.get('timepoints')
+		bulk_timepoints = []
+		for timepoint in recipe_timepoints:
+			new_timepoint = TimePoint(recipe=instance,**timepoint)
+			bulk_timepoints.append(new_timepoint)
+		
+		TimePoint.objects.bulk_create(bulk_timepoints)
+		return instance
+	
+	
+
+
